42408cbaf2499af59eee61c2f9c9a97a54cf6d72
gordon0056 <gordon000056@gmail.com>
2023-07-21 11:26:34 +0300

    eighth attempt
    +added header.cpp
    +simplify smth classes
    +testing lines

--- Header.cpp
+++ Header.cpp
@@ -1,6 +1,6 @@
 #include "Header.h"
 
-void PuzzleSolver::readInputFromFile(const std::string& filename)
+void Game::readInputFromFile(const std::string& filename)
 {
 	std::ifstream inputFile(filename);
 	if (!inputFile.is_open())
@@ -20,31 +20,31 @@ void PuzzleSolver::readInputFromFile(const std::string& filename)
 	}
 
 	for (int i = 0; i < numChips; i++)
-	{ // Исправлено здесь
+	{
 		int chipPosition;
 		inputFile >> chipPosition;
 		chips.emplace_back(i + 1, chipPosition);
 	}
 
 	for (int i = 0; i < numChips; i++)
-	{ // Исправлено здесь
+	{
 		int target;
 		inputFile >> target;
 		chips[i].target = target;
 	}
 
-    inputFile >> numConnections;
+	inputFile >> numConnections;
 
-    for (int i = 0; i < numConnections; i++)
-    {
-        int point1, point2;
-        inputFile >> point1 >> point2;
-        connections.emplace_back(point1, point2);
-    }
-    inputFile.close();
+	for (int i = 0; i < numConnections; i++)
+	{
+		int point1, point2;
+		inputFile >> point1 >> point2;
+		connections.emplace_back(point1, point2);
+	}
+	inputFile.close();
 }
 
-void PuzzleSolver::buildGraph()
+void Game::buildGraph()
 {
 	for (const auto& connection : connections)
 	{
@@ -53,68 +53,157 @@ void PuzzleSolver::buildGraph()
 	}
 }
 
-std::vector<int> PuzzleSolver::findPath(int start, int end)
+void Game::Initialization(const std::string& inputFilename)
 {
-        std::unordered_map<int, bool> visited;
-        std::unordered_map<int, int> prev;
-        std::queue<int> q;
-
-        q.push(start);
-        visited[start] = true;
-
-        while (!q.empty())
-        {
-            int current = q.front();
-            q.pop();
-
-            if (current == end)
-                break; // Путь найден
-
-
-            for (int neighbor : graph[current])
-            {
-                if (!visited[neighbor])
-                {
-                    q.push(neighbor);
-                    visited[neighbor] = true;
-                    prev[neighbor] = current;
-                }
-            }
-        }
-
-        // Восстановление пути
-        std::vector<int> path;
-        int current = end;
-
-        while (current != start)
-        {
-            path.push_back(current);
-            current = prev[current];
-        }
-
-        path.push_back(start);
-        reverse(path.begin(), path.end());
-
-        return path;
+	readInputFromFile(inputFilename);
+	buildGraph();
 }
 
-void PuzzleSolver::solvePuzzle(const std::string& inputFilename)
+void Game::Generation()
 {
-    readInputFromFile(inputFilename);
-    buildGraph();
+	/*PuzzleSolver solver;
+	solver.solvePuzzle("D:\\Source\\Caps\\Caps\\input.txt");*/
 
-    for (const auto& chip : chips)
-    {
-        int start = chip.position;
-        int end = points[chip.id - 1].id;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "sfml chip movement");
 
-        std::vector<int> path = findPath(start, end);
+	const int chipCount = 6;
+	std::vector<sf::CircleShape> chips(chipCount, sf::CircleShape(25.f));
+	std::vector<sf::Vector2f> targets(chipCount, sf::Vector2f(0.f, 0.f));
+	std::vector<bool> isselected(chipCount, false);
+	std::vector<bool> ismoving(chipCount, false);
 
-        // Вывод пути
-        std::cout << "Path for Chip " << chip.id << ": ";
-        for (int point : path)
-            std::cout << point << " ";
+	for (int i = 0; i < chips.size(); ++i)
+	{
+	 chips[i].setFillColor(sf::Color::Green);
+	 chips[i].setOutlineThickness(5.f);
+	 chips[i].setPosition(100.f + i * 100.f, 100.f); // пример расположения фишек
+	}
 
-        std::cout << std::endl;
-    }
+	sf::RectangleShape square(sf::Vector2f(50.f, 250.f));
+	square.setPosition(100, 100);
+	square.setFillColor(sf::Color(180, 180, 180));
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+			{
+				sf::Vector2f mousepos(sf::Mouse::getPosition(window));
+				for (int i = 0; i < chipCount; ++i)
+				{
+					if (chips[i].getGlobalBounds().contains(mousepos))
+					{
+						isselected[i] = !isselected[i];
+						chips[i].setOutlineColor(isselected[i] ? sf::Color::Yellow : sf::Color::White);
+					}
+					else if (isselected[i])
+					{
+						targets[i] = mousepos - sf::Vector2f(25.f, 25.f);
+						ismoving[i] = true;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < chipCount; ++i)
+		{
+			if (ismoving[i])
+			{
+				sf::Vector2f direction = targets[i] - chips[i].getPosition();
+				float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
+				if (length > 1.f)
+				{
+					direction /= length + 1200;
+					chips[i].move(direction);
+				}
+				else
+				{
+					chips[i].setPosition(targets[i]);
+					ismoving[i] = false;
+					isselected[i] = false;
+					chips[i].setOutlineColor(sf::Color::White);
+				}
+			}
+		}
+
+		window.clear(sf::Color::White);
+		window.draw(square);
+		for (const auto& chip : chips)
+			window.draw(chip);
+		window.display();
+	}
+}
+
+std::vector<int> Game::findPath(int start, int end)
+{
+	std::unordered_map<int, bool> visited;
+	std::unordered_map<int, int> prev;
+	std::queue<int> q;
+
+	q.push(start);
+	visited[start] = true;
+
+	while (!q.empty())
+	{
+		int current = q.front();
+		q.pop();
+
+		if (current == end)
+			break;
+
+
+		for (int neighbor : graph[current])
+		{
+			if (!visited[neighbor])
+			{
+				q.push(neighbor);
+				visited[neighbor] = true;
+				prev[neighbor] = current;
+			}
+		}
+	}
+
+	std::vector<int> path;
+	int current = end;
+
+	while (current != start)
+	{
+		path.push_back(current);
+		current = prev[current];
+	}
+
+	path.push_back(start);
+	reverse(path.begin(), path.end());
+
+	return path;
+}
+
+void Game::solvePuzzle(const std::string& inputFilename)
+{
+	readInputFromFile(inputFilename);
+	buildGraph();
+
+	for (const auto& chip : chips)
+	{
+		int start = chip.position;
+		int end = points[chip.id - 1].id;
+
+		std::vector<int> path = findPath(start, end);
+
+		std::cout << "Path for Chip " << chip.id << ": ";
+		for (int point : path)
+			std::cout << point << " ";
+
+		std::cout << std::endl;
+	}
+}
+
+void Game::start()
+{
+	this->Generation();
 }
--- Header.h
+++ Header.h
@@ -8,55 +8,55 @@
 #include <queue>
 #include <SFML/Graphics.hpp>
 
-// Класс для представления фишек
 class Chip
 {
 public:
-    unsigned int id;
-    unsigned int position;
-    unsigned int target;
+	unsigned int id;
+	unsigned int position;
+	unsigned int target;
 
-    Chip(unsigned int id, unsigned int position) : id(id), position(position), target(0) {}
+	Chip(unsigned int id, unsigned int position) : id(id), position(position), target(0) {}
 };
 
-// Класс для представления точек
 class Point
 {
 public:
-    unsigned int id;
-    unsigned int x;
-    unsigned int y;
+	unsigned int id;
+	unsigned int x;
+	unsigned int y;
 
-    Point(unsigned int id, unsigned int x, unsigned int y) : id(id), x(x), y(y) {}
+	Point(unsigned int id, unsigned int x, unsigned int y) : id(id), x(x), y(y) {}
 };
 
-// Класс для представления соединений
 class Connection
 {
 public:
-    unsigned int id1;
-    unsigned int id2;
+	unsigned int id1;
+	unsigned int id2;
 
-    Connection(unsigned int id1, unsigned int id2) : id1(id1), id2(id2) {}
+	Connection(unsigned int id1, unsigned int id2) : id1(id1), id2(id2) {}
 };
 
-//// Класс для построения пути
-class PuzzleSolver
+class Game
 {
 private:
-    std::vector<Chip> chips;
-    std::vector<Point> points;
-    std::vector<Connection> connections;
-    std::unordered_map<int, std::vector<int>> graph;
+	std::vector<Chip> chips;
+	std::vector<Point> points;
+	std::vector<Connection> connections;
+	std::unordered_map<int, std::vector<int>> graph;
 
-    void readInputFromFile(const std::string& filename);
+	void readInputFromFile(const std::string& filename);
 
-    void buildGraph();
+	void buildGraph();
 
-    std::vector<int> findPath(int start, int end);
+	void Initialization(const std::string& inputFilename);
+
+	void Generation();
+
+	std::vector<int> findPath(int start, int end);
 
 public:
 
-    void solvePuzzle(const std::string& inputFilename);
+	void solvePuzzle(const std::string& inputFilename);
+	void start();
 };
-
